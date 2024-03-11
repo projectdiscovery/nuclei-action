bc9f21b27f177374ebc0a32878a2025eb8e934cf
Felipe Saez <felipe.saez@shopify.com>
2024-03-11 13:09:38 -0300

    Fix regex for flags

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 export function parseFlagsToArray(rawFlags) {
-    const re = /(?:(?:^|\s)-[-a-z]+)(?:(?:\s|=)(?:[^-][0-9a-z-\S]+))?/g;
+    const re = /(?:(?:^|\s)-[-a-z]+)(?:(?:\s|=)(?:[^-](?:[0-9a-z-\S])*))?/g;
     return rawFlags.match(re).map(token => token.trim()).map(token => token.replace(' ', '='));
 }
